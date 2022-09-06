9a190b2b2d56f9eebae92c86efc5c153cf8e37b7
IgnaBarrenechea <ibarrenechea79@gmail.com>
2022-09-05 22:28:11 -0300

    Para entregar

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo");
-    });
+})
 
 app.get("/", function(req, res){
     res.sendFile(path.resolve(__dirname, './views/index.html'));
@@ -15,6 +15,18 @@ app.get("/", function(req, res){
 
 app.get("/perfil", function(req, res){
     res.sendFile(path.resolve(__dirname, './views/perfil.html'));
+});
+
+app.get("/login", function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+app.get("/producto", function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/producto.html'));
+});
+
+app.get("/registro", function(req, res){
+    res.sendFile(path.resolve(__dirname, './views/registro.html'));
 });
 
 app.get('*',function(req,res){
