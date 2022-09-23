ff232d6b341efc22a82f06c2adfcc93c7f161ea7
IgnaBarrenechea <ibarrenechea79@gmail.com>
2022-09-23 10:20:26 -0300

    Creé la carpeta routes, contrllers e instale nodemon

---
+++ controllers/productoControllers.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+let productoControllers = {
+    listadoProductos: function (req,res){
+        console.log ("estoy?")
+    }
+}
+/*
+
+});
+*/
+
+module.exports = productoControllers
