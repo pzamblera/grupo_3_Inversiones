5bcdfca21efb9f9fcbdd6dad1220d804de0bacad
IgnaBarrenechea <ibarrenechea79@gmail.com>
2022-11-08 15:53:58 -0300

    Se arreglo la ruta POST que estaba dañada

--- src/routes/main.js
+++ src/routes/main.js
@@ -5,10 +5,11 @@ const path = require('path');
 const controller = require("../controllers/mainControllers");
 const {body, check} = require("express-validator");
 
-const validaciones = [
+/* const validaciones = [
     body("correoElectronico").isEmail().withMessage("Completar al campo con un mail válido"),
     body("contrasenia").notEmpty(),
-]; // es un middle que se encarga de validar lo que se cargue en perfil.
+]; */ 
+// es un middle que se encarga de validar lo que se cargue en perfil.
 
 const multerDiskStorage = multer.diskStorage({
     destination: function(req, file, cb) {       // request, archivo y callback que almacena archivo en destino
@@ -24,10 +25,10 @@ const uploadFile = multer({ storage: multerDiskStorage });
 
 router.get("/", controller.index)
 router.get("/login", controller.login)
-router.post("/login", [
-    check("email").isEmail().withMessage("email invalido"),
-    check("password").isLength({min:8}).withMessage("La contraseña debe tener al menos 8 caracteres")
-] ,controller.processLogin)
+/* router.post("/login", [
+   check("email").isEmail().withMessage("email invalido"),
+   check("password").isLength({min:8}).withMessage("La contraseña debe tener al menos 8 caracteres"),
+], controller.processLogin) */
 router.get("/registro", controller.registro)
 router.post("/registro", uploadFile.single("avatar"), controller.registro2)
 router.get("/perfil", controller.perfil)
