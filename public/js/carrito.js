34e36117bae5bda040eabc91afc739d274e976c7
IgnaBarrenechea <ibarrenechea79@gmail.com>
2022-12-19 15:25:32 -0300

    n/a

---
+++ public/js/carrito.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function(){
+    document.getElementById("carrito").addEventListener("click", function(event){
+        event.preventDefault();
+        let a = 
+        sessionStorage.setItem("monto", a)
+    });
+})
+
